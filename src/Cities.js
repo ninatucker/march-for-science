92a66ca2931c8bce632b82bedbd2e5c37273ba57
Elliot Charney <elliot.charney@ithaka.org>
2017-02-20 18:18:18 -0500

    alphabet is a thing

--- src/Cities.js
+++ src/Cities.js
@@ -1,14 +1,16 @@
 export default [
     {
-        name: "Lansing",
+        name: "Ann Arbor",
         date: "April 22, 2017",
-        time: "1PM - 4PM",
-        location: "the Michigan State Capitol",
-        address: "110 N Capitol Ave, Lansing, Michigan 48933",
-        facebookEvent: "https://www.facebook.com/events/985534291591681/",
-        hashtag: "#MarchForScienceLansing",
-        url: "lansing",
-        mapIframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2930.5725890995295!2d-84.55606208469565!3d42.7339364194009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8822c1d7f9080847%3A0x50dbccd326435ec7!2s110+N+Capitol+Ave%2C+Lansing%2C+MI+48933!5e0!3m2!1sen!2sus!4v1487619752713",
+        time: "3PM - 6PM",
+        location: "TBD",
+        address: "",
+        facebookEvent: "https://www.facebook.com/events/1237144609734310/",
+        hashtag: "",
+        email: "MarchForScienceA2@gmail.com",
+        twitter: "ScienceMarchA2",
+        url: "annarbor",
+        mapIframe: "",
         mapUrl: ""
     },
     {
@@ -24,6 +26,20 @@ export default [
         mapUrl: ""
     },
     {
+        name: "Houghton / Hancock",
+        date: "April 22, 2017",
+        time: "11AM - 1PM",
+        location: "the Houghton Hancock Lift Bridge",
+        address: "",
+        facebookEvent: "https://www.facebook.com/events/1710658009264726/",
+        hashtag: "",
+        email: "MarchforScienceHoughton@gmail.com",
+        twitter: "M4SHoughton",
+        url: "houghton",
+        mapIframe: "",
+        mapUrl: ""
+    },
+    {
         name: "Kalamazoo",
         date: "April 22, 2017",
         time: "12PM - 3PM",
@@ -33,6 +49,18 @@ export default [
         hashtag: "",
         url: "kalamazoo",
         mapIframe: "",
+        mapUrl: ""
+    },
+    {
+        name: "Lansing",
+        date: "April 22, 2017",
+        time: "1PM - 4PM",
+        location: "the Michigan State Capitol",
+        address: "110 N Capitol Ave, Lansing, Michigan 48933",
+        facebookEvent: "https://www.facebook.com/events/985534291591681/",
+        hashtag: "#MarchForScienceLansing",
+        url: "lansing",
+        mapIframe: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2930.5725890995295!2d-84.55606208469565!3d42.7339364194009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8822c1d7f9080847%3A0x50dbccd326435ec7!2s110+N+Capitol+Ave%2C+Lansing%2C+MI+48933!5e0!3m2!1sen!2sus!4v1487619752713",
         mapUrl: ""
     },
     {
@@ -56,34 +84,6 @@ export default [
         facebookEvent: "https://www.facebook.com/events/1869079686704228",
         hashtag: "",
         url: "sault",
-        mapIframe: "",
-        mapUrl: ""
-    },
-    {
-        name: "Houghton / Hancock",
-        date: "April 22, 2017",
-        time: "11AM - 1PM",
-        location: "the Houghton Hancock Lift Bridge",
-        address: "",
-        facebookEvent: "https://www.facebook.com/events/1710658009264726/",
-        hashtag: "",
-        email: "MarchforScienceHoughton@gmail.com",
-        twitter: "M4SHoughton",
-        url: "houghton",
-        mapIframe: "",
-        mapUrl: ""
-    },
-    {
-        name: "Ann Arbor",
-        date: "April 22, 2017",
-        time: "3PM - 6PM",
-        location: "TBD",
-        address: "",
-        facebookEvent: "https://www.facebook.com/events/1237144609734310/",
-        hashtag: "",
-        email: "MarchForScienceA2@gmail.com",
-        twitter: "ScienceMarchA2",
-        url: "annarbor",
         mapIframe: "",
         mapUrl: ""
     }
