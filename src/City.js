e9c6022d4b6f0380c9271602234a01360ff9823f
siromivel <siromivel@gmail.com>
2017-02-19 22:14:18 -0500

    Add copy, update City.js
    - Adds fancy array handling merge-o-matic stuffs for URL parsing.
    - Adds some basic copy

--- src/City.js
+++ src/City.js
@@ -2,5 +2,9 @@ import Cities from './Cities';
 
 // whatever you export from this module will be the
 // city that the app references
+let getCity = function getCity(city) {
+  return Cities.find(c => c.url === city) || Cities.find(c => c.url === 'annarbor');
+}
 
-export default Cities.lansing;
+let current = location && location.pathname.length > 1 ? location.pathname.match(/\/(.[^/]+)/)[1] : 'annarbor';
+export default getCity(current) ? getCity(current) : getCity("annarbor");
--- src/components/ModuleTwo/ModuleTwo.js
+++ src/components/ModuleTwo/ModuleTwo.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
 import styles from './ModuleTwo.css';
 
 import FaFlask from 'react-icons/lib/fa/flask';
+import FaBinoculars from 'react-icons/lib/fa/binoculars';
 
 class Hero extends Component {
     render() {
@@ -13,19 +13,18 @@ class Hero extends Component {
                         <FaFlask style={styles.icon} />
                     </div>
                     <div className="content">
-                    <h2>Magna primis lobortis<br />
-                        sed ullamcorper</h2>
-                    <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+                    <p>The March for Science is a celebration of our passion for science and a call to support and safeguard the scientific community. Recent policy changes have caused heightened worry among scientists, and the incredible and immediate outpouring of support has made clear that these concerns are also shared by hundreds of thousands of people around the world. The mischaracterization of science as a partisan issue, which has given policymakers permission to reject overwhelming evidence, is a critical and urgent matter. It is time for people who support scientific research and evidence-based policies to take a public stand and be counted.</p>
                 </div>
                 </section>
                 <section className="spotlight">
                     <div className="image" style={styles.iconContainer}>
-                        <FaFlask style={styles.icon} />
+                        <FaBinoculars style={styles.icon} />
                     </div>
                     <div className="content">
-                    <h2>Tortor dolore feugiat<br />
-                        elementum magna</h2>
-                    <p>Aliquam ut ex ut augue consectetur interdum. Donec hendrerit imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.</p>
+                    <h2>WHY A MARCH?</h2>
+                    <p>
+                        A recent focus on science and how scientific findings impact public policy has put a spotlight on the scientific community and their research. We accept that science, as a field of knowledge and way of thinking, is not swayed by politics or opinions; it is nonpartisan and all-inclusive. This is the time for us to come together as one community, take a stand, and lead a new conversation.
+                    </p>
                 </div>
                 </section>
                 <section className="spotlight">
