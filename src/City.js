fcb2fadafd98427b0acb6a2c72ec2fc767b8b26c
siromivel <siromivel@gmail.com>
2017-02-19 21:06:06 -0500

    Fix base URL

--- src/City.js
+++ src/City.js
@@ -2,6 +2,6 @@ import Cities from './Cities';
 
 // whatever you export from this module will be the
 // city that the app references
-let current = location ? location.pathname.match(/\/(.[^/]+)/)[1] : "annarbor";
+let current = location && location.pathname.length > 1 ? location.pathname.match(/\/(.[^/]+)/)[1] : "annarbor";
 
 export default Cities[current] ? Cities[current] : Cities["annarbor"];
